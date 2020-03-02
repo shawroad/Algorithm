cf85bafd77051b7c78ee4dec6b2632c0fce50059
肖路 <shawroad@MacBook-ProTCL.local>
2020-03-02 12:58:21 +0800

    2020-3-2

---
+++ 备战2020/058-路径总和.py
@@ -0,0 +1,44 @@
+"""
+
+@file   : 058-路径总和.py
+
+@author : xiaolu
+
+@time   : 2020-03-02
+
+"""
+'''
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+'''
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        sum -= root.val
+        if not root.left and not root.right:
+            return sum == 0
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+
