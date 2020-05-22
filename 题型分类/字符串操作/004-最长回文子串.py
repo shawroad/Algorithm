5fa8ac721b814b2d658819ee9907a25088b3524b
XL <lu.xiao@tcl.com>
2020-05-22 09:01:28 +0800

    2020-5-22

---
+++ 题型分类/字符串操作/004-最长回文子串.py
@@ -0,0 +1,75 @@
+"""
+
+@file  : 004-最长回文子串.py
+
+@author: xiaolu
+
+@time  : 2020-05-21
+
+"""
+'''
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+
+示例 2：
+输入: "cbbd"
+输出: "bb"
+'''
+
+
+# 中心扩充算法
+class Solution:
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    def longestPalindrome(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            left2, right2 = self.expandAroundCenter(s, i, i+1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
+
+
+# 方法二: 动态规划
+class Solution2:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 l+1
+        for l in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i+1][j-1] and s[i] == s[j])
+
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+
+
+if __name__ == '__main__':
+    s = 'babad'
+    sol = Solution2()
+    result = sol.longestPalindrome(s)
+    print(result)
+
