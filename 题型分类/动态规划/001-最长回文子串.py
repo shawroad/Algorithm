9386dadad564b496da89d6fb61805768b7d569b8
肖路 <shawroad@MacBook-ProTCL.local>
2020-06-07 13:45:21 +0800

    2020-6-8

---
+++ 题型分类/动态规划/001-最长回文子串.py
@@ -0,0 +1,50 @@
+"""
+
+@file   : 001-最长回文子串.py
+
+@author : xiaolu
+
+@time   : 2020-06-07
+
+"""
+'''
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+
+示例 2：
+输入: "cbbd"
+输出: "bb"
+'''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 l+1
+        for l in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+
+
+if __name__ == '__main__':
+    s = 'babad'
+    sol = Solution()
+    result = sol.longestPalindrome(s)
+    print(result)
+
